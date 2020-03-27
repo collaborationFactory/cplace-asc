ce1481116c7d55c2c80859a6a87f149a5897ac8c
Michael Rose <michael.rose@collaboration-factory.de>
2020-03-27 17:34:45 +0100

    add potential solution hint

--- src/model/PluginDescriptorParser.ts
+++ src/model/PluginDescriptorParser.ts
@@ -27,6 +27,9 @@ export class PluginDescriptorParser {
         this.pathToDescriptor = PluginDescriptorParser.getPathToDescriptor(pluginDir);
         if (!fs.existsSync(this.pathToDescriptor)) {
             console.error(cerr`(PluginDescriptor) Failed to find plugin descriptor for ${path.basename(pluginDir)}`);
+            console.error();
+            console.error(cerr`(PluginDescriptor) Try running "gradle generatePluginDescriptor" in the containing repository`);
+            console.error();
             throw Error(`PluginDescriptor ${this.pathToDescriptor} does not exist`);
         }
         this.descriptor = this.parseFile();
