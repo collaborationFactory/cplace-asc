a2e76aff6fc4c54736b9504e4d456277102ff0d0
Slaven Kopic <slaven.kopic@collaboration-factory.de>
2021-03-19 10:55:51 +0100

    styles bundler added

--- src/compiler/VendorCompiler.ts
+++ src/compiler/VendorCompiler.ts
@@ -8,15 +8,17 @@ import * as webpack from "webpack";
 import spawn = require("cross-spawn");
 import * as crypto from "crypto";
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 export class VendorCompiler implements ICompiler {
     public static readonly DEST_JS_DIR = 'generated_js';
-    public static readonly DEST_CSS_DIR = 'generated_css';
+    public static readonly DEST_CSS_DIR = 'css';
     private static readonly VENDOR_ENTRY = 'index.js';
     private static readonly VENDOR_ENTRY_HASH = 'index.js.hash';
     private static readonly VENDOR_JS_FILE = 'vendor.js';
     private static readonly VENDOR_CSS_FILE = 'vendor.css';
-    private static readonly JAVASCRIPT_TO_BE_COMPRESSED = 'javaScriptIncludesToBeCompressed.txt'
+    private static readonly JAVASCRIPT_TO_BE_COMPRESSED = 'javaScriptIncludesToBeCompressed.txt';
+    private static readonly CSS_IMPORTS = 'imports.css';
 
     constructor(private readonly pluginName: string,
                 private readonly dependencyPaths: string[],
@@ -35,16 +37,17 @@ export class VendorCompiler implements ICompiler {
 
         this.tscPluginIndex();
 
-        const oldHash = this.readIndexHash();
-        const newHash = this.createIndexHashFile();
+        const oldIndexHash = this.readIndexHash();
+        const newIndexHash = this.createIndexHashFile();
 
-        if (oldHash === newHash && !dependenciesWereUpdated) {
+        if (oldIndexHash === newIndexHash && !dependenciesWereUpdated) {
             console.log(cgreen`✓`, `[${this.pluginName}] vendors are up to date`);
             return Promise.resolve(CompilationResult.CHANGED);
         }
 
         await this.bundlePluginVendors();
-        await this.prepareVendorsForCompression(this.assetsPath);
+        this.prepareVendorJSForCompression();
+        this.prepareVendorCSSForCompression();
 
         const endTime = new Date().getTime();
         console.log(cgreen`✓`, `[${this.pluginName}] vendors compiled (${formatDuration(endTime - startTime)})`);
@@ -127,16 +130,26 @@ export class VendorCompiler implements ICompiler {
                 path: path.resolve(this.assetsPath, VendorCompiler.DEST_JS_DIR),
                 filename: VendorCompiler.VENDOR_JS_FILE
             },
+            resolveLoader: {
+                modules: [path.resolve(__dirname, '../../', 'node_modules')]
+            },
             resolve: {
-                modules: ['node_modules']
+                modules: [path.resolve(this.assetsPath, 'node_modules')]
             },
             optimization: {
-                minimize: true
+                minimize: true,
+                minimizer: [
+                    new OptimizeCSSAssetsPlugin({
+                        cssProcessorPluginOptions: {
+                            preset: ['default', { discardComments: { removeAll: true } }],
+                        }
+                    })
+                ]
             },
             devtool: false,
             plugins: [
                 new MiniCssExtractPlugin({
-                    filename: path.resolve(this.assetsPath, `${VendorCompiler.DEST_CSS_DIR}/${VendorCompiler.VENDOR_CSS_FILE}`)
+                    filename: `../${VendorCompiler.DEST_CSS_DIR}/${VendorCompiler.VENDOR_CSS_FILE}`
                 })
             ],
             module: {
@@ -166,10 +179,11 @@ export class VendorCompiler implements ICompiler {
             const config = this.getPluginWebpackConfig();
 
             // remove previously generated webpack bundle if exists (so it does not append)
-            const bundleFile = path.resolve(this.assetsPath, VendorCompiler.DEST_JS_DIR, VendorCompiler.VENDOR_JS_FILE)
-            if (fs.existsSync(bundleFile)) {
-                fs.unlinkSync(bundleFile);
-            }
+            const vendorJsFile = path.resolve(this.assetsPath, VendorCompiler.DEST_JS_DIR, VendorCompiler.VENDOR_JS_FILE)
+            const vendorCssFile = path.resolve(this.assetsPath, VendorCompiler.DEST_CSS_DIR, VendorCompiler.VENDOR_CSS_FILE);
+
+            this.removeFileIfExists(vendorJsFile);
+            this.removeFileIfExists(vendorCssFile);
 
             webpack(config, (err, stats) => {
                 if (err) {
@@ -187,38 +201,112 @@ export class VendorCompiler implements ICompiler {
 
     /**
      * Writes vendor.js import to javaScriptIncludesToBeCompressed.txt
-     * @param assetsPath Provided assets path
      * @private
      */
-    private prepareVendorsForCompression(assetsPath: string): Promise<any> {
+    private prepareVendorJSForCompression(): void {
+        const vendorJsPath = path.resolve(this.assetsPath, VendorCompiler.DEST_JS_DIR, VendorCompiler.VENDOR_JS_FILE);
+        const javaScriptToBeCompressedPath = path.join(this.assetsPath, VendorCompiler.JAVASCRIPT_TO_BE_COMPRESSED);
+        const pathToInclude = `/${VendorCompiler.DEST_JS_DIR}/${VendorCompiler.VENDOR_JS_FILE}`;
 
-        return new Promise<any>((resolve, reject) => {
-            const javaScriptToBeCompressedPath = path.join(assetsPath, VendorCompiler.JAVASCRIPT_TO_BE_COMPRESSED);
+        const noJsVendor = this.cleanVendor(vendorJsPath, javaScriptToBeCompressedPath, pathToInclude);
 
-            fs.readFile(javaScriptToBeCompressedPath, 'utf8', (err, buff) => {
+        if (noJsVendor) {
+            return;
+        }
+        this.writeVendorImport(javaScriptToBeCompressedPath, pathToInclude);
+    }
 
-                if (err) {
-                    reject(`Error reading ${javaScriptToBeCompressedPath}`);
+    /**
+     * Writes vendor.js import to javaScriptIncludesToBeCompressed.txt
+     * @private
+     */
+    private prepareVendorCSSForCompression(): void {
+        const vendorCssPath = path.join(this.assetsPath, VendorCompiler.DEST_CSS_DIR, VendorCompiler.VENDOR_CSS_FILE);
+        const cssImportsPath = path.join(this.assetsPath, VendorCompiler.DEST_CSS_DIR, VendorCompiler.CSS_IMPORTS);
+        const pathToInclude = `@import url("./${VendorCompiler.VENDOR_CSS_FILE}");`;
+
+        const noCssVendor = this.cleanVendor(vendorCssPath, cssImportsPath, pathToInclude);
+
+        if (noCssVendor) {
+            return;
+        }
+        this.writeVendorImport(cssImportsPath, pathToInclude);
+    }
+
+    /**
+     * Writes vendor.(css|js) import to correct file
+     * @param fileToWrite File the import needs to be written
+     * @param pathToInclude Import path to include
+     * @private
+     */
+    private writeVendorImport(fileToWrite: string, pathToInclude: string): void {
+        let buffer = '';
+
+        if (fs.existsSync(fileToWrite)) {
+            buffer = fs.readFileSync(fileToWrite, 'utf8');
+
+            if (buffer.includes(pathToInclude)) {
+                // removes included path if already exists
+                const includedPaths = buffer.split('\n');
+                const index = includedPaths.indexOf(pathToInclude);
+                includedPaths.splice(index, 1);
+                buffer = includedPaths.join('\n');
+            }
+        }
+
+        const content = buffer + `\n${pathToInclude}`;
+        fs.writeFileSync(fileToWrite, content);
+    }
+
+    /**
+     * Cleans vendor imports
+     * @param vendorFilePath Generated vendor file path
+     * @param fileToWrite File to write vendor import
+     * @param pathToInclude Import path to include
+     * @private
+     */
+    private cleanVendor(vendorFilePath: string, fileToWrite: string, pathToInclude: string): boolean {
+        if (!fs.existsSync(vendorFilePath)) {
+
+            // if there is no file to write import to, skip further process
+            if (!fs.existsSync(fileToWrite)) {
+                return true;
+            }
+
+            let buffer = fs.readFileSync(fileToWrite, 'utf8');
+
+            // if file to write import to, doesn't have any content, remove the file
+            if (!buffer.length) {
+                this.removeFileIfExists(fileToWrite);
+                return true;
+            }
+
+            if (buffer.includes(pathToInclude)) {
+                // removes included path if already exists
+                const includedPaths = buffer.split('\n');
+                const index = includedPaths.indexOf(pathToInclude);
+                // if file to write import to includes vendor import and there is no vendor, remove the import line
+                includedPaths.splice(index, 1);
+                buffer = includedPaths.join('\n');
+                if (!buffer.length) {
+                    this.removeFileIfExists(fileToWrite);
+                    return true;
                 }
+                fs.writeFileSync(fileToWrite, buffer);
+            }
+            return true;
+        }
+        return false;
+    }
 
-                const pathToInclude = `/${VendorCompiler.DEST_JS_DIR}/${VendorCompiler.VENDOR_JS_FILE}`;
-                if (buff.includes(pathToInclude)) {
-                    // removes included path if already exists
-                    const includedPaths = buff.split('\n');
-                    const index = includedPaths.indexOf(pathToInclude);
-                    includedPaths.splice(index, 1);
-                    buff = includedPaths.join('\n');
-                }
-
-                const content = buff + `\n${pathToInclude}`;
-
-                fs.writeFile(javaScriptToBeCompressedPath, content, (e) => {
-                    if (e) {
-                        reject(`Error writing ${pathToInclude} to ${javaScriptToBeCompressedPath}`);
-                    }
-                    resolve(true);
-                });
-            });
-        });
+    /**
+     * Removes the file, if the file exists
+     * @param filePath File to remove
+     * @private
+     */
+    private removeFileIfExists(filePath: string): void {
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+        }
     }
 }
