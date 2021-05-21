cfb0eceb09e5c057e600f5f570c449c09a7181b8
Slaven Kopic <slaven.kopic@collaboration-factory.de>
2021-05-21 15:10:59 +0200

    code improved

--- src/model/LessEscapePlugin.ts
+++ src/model/LessEscapePlugin.ts
@@ -19,7 +19,7 @@ function checkImproperLessEscaping(cssOutput: string): string | undefined {
     }
 }
 
-let processedFiles: string[] = [];
+let processedFiles: Set<string> = new Set<string>();
 
 /**
  * Processes LESS data and warns if there is an improper LESS escaping
@@ -29,14 +29,18 @@ export function lessEscapePlugin(pluginName: string) {
     return {
         process: (src, extra) => {
             const improperLessEscaping = checkImproperLessEscaping(src);
-            if (improperLessEscaping && !processedFiles.includes(extra.fileInfo.filename)) {
-                const spacer = '  ';
-                console.log(cwarn`⇢ [${pluginName}] LESS not properly escaped in ${extra.fileInfo.filename}:\n${improperLessEscaping}\n\n${spacer}Please escape LESS the following way:\n${spacer}width: ~"calc(100% - 200px)"; or width: ~"calc(100vw - " @yourVariable ~")";\n`);
-                processedFiles.push(extra.fileInfo.filename);
+            if (improperLessEscaping && !processedFiles.has(extra.fileInfo.filename)) {
+                const spacer = ' ';
+                console.log(
+                    cwarn`⇢ [${pluginName}] LESS not properly escaped in ${extra.fileInfo.filename}:`,
+                    cwarn`\n${improperLessEscaping}\n\n`,
+                    cwarn`${spacer}Please escape LESS the following way:\n`,
+                    cwarn`${spacer}width: ~"calc(100% - 200px)"; or width: ~"calc(100vw - " @yourVariable ~")";\n`
+                );
+                processedFiles.add(extra.fileInfo.filename);
             } else {
-                const foundFile = processedFiles.indexOf(extra.fileInfo.filename);
-                if (foundFile !== -1) {
-                    processedFiles.splice(foundFile, 1);
+                if (processedFiles.has(extra.fileInfo.filename)) {
+                    processedFiles.delete(extra.fileInfo.filename);
                 }
             }
             return src;
