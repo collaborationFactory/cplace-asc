5feb3281de6063a41627814e3542662aeadb1725
vladimir-arsov <vladimir.arsov@collaboration-factory.de>
2022-07-22 13:05:43 +0200

    Add tests with simple and extended plugin descriptor

---
+++ test/PluginDescriptor.test.ts
@@ -0,0 +1,115 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as tmp from 'tmp';
+import { AssetsCompiler, IAssetsCompilerConfiguration } from '../src/model/AssetsCompiler';
+import { PackageVersion } from '../src/model/PackageVersion';
+import { generateBuildGradleFile, generateExtendedPluginDescriptor, generatePackageJson, generateParentRepos, generateSimplePluginDescriptor } from './helper/TestHelpers';
+
+describe('test the handling of plugin descriptor', () => {
+    const mainRepoName = 'main';
+    const otherRepoName = 'cplace-paw';
+    let platformPath;
+    let pluginPath;
+
+    let tmpTestFolder: tmp.DirSyncObject;
+    let basePath: string;
+    let mainRepoPath: string;
+    let otherRepoPath: string;
+
+    let currentCwd;
+
+    beforeEach(() => {
+        tmpTestFolder = tmp.dirSync({ unsafeCleanup: true });
+        console.log('Test data will be below: ', tmpTestFolder.name);
+        basePath = tmpTestFolder.name;
+        mainRepoPath = path.join(basePath, mainRepoName);
+        otherRepoPath = path.join(basePath, otherRepoName);
+
+        // generate main repo and platform
+        fs.mkdirSync((mainRepoPath), {
+            recursive: true,
+        });
+        platformPath = path.resolve(mainRepoPath, 'cf.cplace.platform');
+        fs.mkdirSync(platformPath);
+        fs.mkdirSync(path.resolve(mainRepoPath, 'cf.cplace.platform', 'src'));
+        fs.mkdirSync(path.resolve(mainRepoPath, 'cf.cplace.platform', 'assets'));
+
+        generateBuildGradleFile(platformPath, []);
+        generatePackageJson(mainRepoPath, "cplace", "3.0.0");
+
+        // generate other repo and plugin
+        fs.mkdirSync((otherRepoPath), {
+            recursive: true,
+        });
+        pluginPath = path.resolve(otherRepoPath, 'cf.cplace.plugin');
+        fs.mkdirSync(pluginPath);
+        fs.mkdirSync(path.resolve(otherRepoPath, 'cf.cplace.plugin', 'src'));
+        fs.mkdirSync(path.resolve(otherRepoPath, 'cf.cplace.plugin', 'assets'));
+        generateBuildGradleFile(pluginPath, ["cf.cplace.platform"]);
+        generateParentRepos(otherRepoPath, ["main"]);
+
+        currentCwd = process.cwd();
+        process.chdir(otherRepoPath);
+    });
+
+    afterEach(() => {
+        process.chdir(currentCwd);
+        tmpTestFolder.removeCallback();
+    });
+
+    test('test assets compiler with simple plugin descriptors', () => {
+        generateSimplePluginDescriptor(platformPath, "cf.cplace.platform", []);
+        generateSimplePluginDescriptor(pluginPath, "cf.cplace.plugin", ["cf.cplace.platform"]);
+
+        const config: IAssetsCompilerConfiguration = {
+            rootPlugins: [],
+            watchFiles: false,
+            onlyPreprocessing: false,
+            clean: false,
+            maxParallelism: 1,
+            localOnly: false,
+            production: false,
+            noParents: false,
+        };
+
+        PackageVersion.initialize(mainRepoPath);
+        const assetsCompiler = new AssetsCompiler(config, otherRepoPath);
+        assetsCompiler.start().then(
+            () => {
+                console.log("Test sucessful");
+            },
+            () => {
+                fail("Cannot execute assets compiler");
+            }
+        );
+
+    });
+
+    test('test assets compiler with extended plugin descriptors', () => {
+        generateExtendedPluginDescriptor(platformPath, "cf.cplace.platform", "cf.cplace", []);
+        generateExtendedPluginDescriptor(pluginPath, "cf.cplace.plugin", "cf.cplace", [{name: "cf.cplace.platform", group: "cf.cplace"}]);
+
+        const config: IAssetsCompilerConfiguration = {
+            rootPlugins: [],
+            watchFiles: false,
+            onlyPreprocessing: false,
+            clean: false,
+            maxParallelism: 1,
+            localOnly: false,
+            production: false,
+            noParents: false,
+        };
+
+        const assetsCompiler = new AssetsCompiler(config, otherRepoPath);
+        assetsCompiler.start().then(
+            () => {
+                console.log("Test sucessful");
+            },
+            () => {
+                fail("Cannot execute assets compiler");
+            }
+        );
+
+    });
+
+});
---
+++ test/helper/TestHelpers.ts
@@ -0,0 +1,59 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function generateBuildGradleFile(pluginPath: string, pluginDependencies: string[]): void {
+    let content = "";
+    content += "dependencies {\n";
+    pluginDependencies.forEach((dependency) => {
+        content += `    cpalcePLugin ${dependency}\n`
+    });
+
+    content += "}\n";
+
+    fs.writeFileSync(path.resolve(pluginPath, 'build.gradle'), content, {
+        encoding: 'utf8',
+    });
+}
+
+export function generateSimplePluginDescriptor(pluginPath: string, name: string, pluginDependencies: string[]): void {
+    let content = {
+        name: name,
+        dependencies: pluginDependencies
+    }
+
+    fs.writeFileSync(path.resolve(pluginPath, 'pluginDescriptor.json'), JSON.stringify(content, null, 4), {
+        encoding: 'utf8',
+    });
+}
+
+export function generateExtendedPluginDescriptor(pluginPath: string, name: string, group: string, pluginDependencies: any[]): void {
+    let content = {
+        name: name,
+        group: group,
+        dependencies: pluginDependencies
+    }
+
+    fs.writeFileSync(path.resolve(pluginPath, 'pluginDescriptor.json'), JSON.stringify(content, null, 4), {
+        encoding: 'utf8',
+    });
+}
+
+export function generatePackageJson(location: string, name: string, version: string): void {
+    let content = {
+        name: name,
+        version: version
+    }
+
+    fs.writeFileSync(path.resolve(location, 'package.json'), JSON.stringify(content, null, 4), {
+        encoding: 'utf8',
+    });
+}
+
+export function generateParentRepos(location: string, names: string[]): void {
+    let content = {}
+    names.forEach((name) => content[`${name}`] = {})
+
+    fs.writeFileSync(path.resolve(location, 'parent-repos.json'), JSON.stringify(content, null, 4), {
+        encoding: 'utf8',
+    });
+}
