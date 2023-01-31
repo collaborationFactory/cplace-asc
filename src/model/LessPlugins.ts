9b94bacda0a6eb23b6a3d034a994cf129b2c950d
Slaven Kopic <slaven.kopic@collaboration-factory.de>
2023-01-31 11:22:59 +0100

    tsc fixed

--- src/model/LessPlugins.ts
+++ src/model/LessPlugins.ts
@@ -4,7 +4,7 @@ import { lessEscapePlugin } from './LessEscapePlugin';
  * Exports all the less plugins
  * @param pluginName Plugin name
  */
-export function lessPlugins(pluginName: string): Less.Plugin[] {
+export function lessPlugins(pluginName: string) {
     return [
         {
             install: (less, pluginManager) => {
