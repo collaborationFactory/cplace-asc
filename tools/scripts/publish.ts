b9e6c07f7fc5eb702a556380fad2fd83b103f1b3
Slaven Kopic <slaven.kopic@collaboration-factory.de>
2023-01-31 09:43:27 +0100

    ts-node path fixed

--- tools/scripts/publish.ts
+++ tools/scripts/publish.ts
@@ -13,8 +13,9 @@ const isSnapshot = version?.includes('SNAPSHOT');
 
 console.log(`Building cplace-asc...`);
 const buildScriptPath = resolve(__dirname, 'build.ts');
-const tsNodeBin = resolve('node_modules/.bin/ts-node');
-console.log(execSync(`${tsNodeBin} ${buildScriptPath} ${version}`).toString());
+console.log(
+    execSync(`npm run ts-node ${buildScriptPath} ${version}`).toString()
+);
 console.log(`cplace-asc successfully built!`);
 process.chdir(CPLACE_ASC_DIST);
 console.log(`Publishing cplace-asc...`);
