ae6eb75d4de3aa5c98aa4315235a1db7ef8367f9
stefancplace <stefan.stadler@collaboration-factory.de>
2023-12-12 16:46:10 +0100

    added null check

--- src/license/LicenseInfos.ts
+++ src/license/LicenseInfos.ts
@@ -38,7 +38,10 @@ export abstract class LicenseInfo {
             this._licenseText = this._licenseText.concat(
                 'License: ' + this.libraryLicenseInfo.license + '    '
             );
-            if (this.libraryLicenseInfo.additionalLicenses.length > 0) {
+            if (
+                this.libraryLicenseInfo.additionalLicenses &&
+                this.libraryLicenseInfo.additionalLicenses.length > 0
+            ) {
                 this._licenseText = this._licenseText.concat(
                     'Additional Licenses: ' +
                         this.libraryLicenseInfo.additionalLicenses.join(', ') +
