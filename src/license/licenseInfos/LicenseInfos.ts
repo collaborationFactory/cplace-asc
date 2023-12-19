e4cfe95d3fca05e7c70e7fb3338f9ca13953a23e
Stefan Stadler <stefan.stadler@cplace.com>
2023-12-19 08:36:40 +0100

    PFM-ISSUE-25539 Added License Text to compressed js in 2.1.x (#131)
    Resolves PFM-ISSUE-25539

---
+++ src/license/licenseInfos/LicenseInfos.ts
@@ -0,0 +1,44 @@
+export interface LibraryLicenseInfo {
+    product: string;
+    component: string;
+    copyright: string;
+    license: string;
+    additionalLicenses: string[];
+    licenseFile: string;
+    licenseText: string;
+}
+
+export abstract class LicenseInfo {
+    get licenseText(): string {
+        return this._licenseText;
+    }
+
+    private _licenseText = '';
+    private libraryLicenseInfo: LibraryLicenseInfo | undefined;
+
+    constructor(libraryLicenseInfo: LibraryLicenseInfo) {
+        this.libraryLicenseInfo = libraryLicenseInfo;
+        this.generateLicenseTexts();
+    }
+
+    public generateLicenseTexts(): void {
+        if (this.libraryLicenseInfo) {
+            this._licenseText = this._licenseText.concat(
+                '// [Component] ' +
+                    this.libraryLicenseInfo.product +
+                    ' ' +
+                    this.libraryLicenseInfo.component +
+                    '    '
+            );
+            this._licenseText = this._licenseText.concat(
+                '[Copyright] ' + this.libraryLicenseInfo.copyright + '    '
+            );
+            this._licenseText = this._licenseText.concat(
+                '[License] ' + this.libraryLicenseInfo.license + '    '
+            );
+            this._licenseText = this._licenseText.concat(
+                '[License Text] ' + this.libraryLicenseInfo.licenseText + '    '
+            );
+        }
+    }
+}
