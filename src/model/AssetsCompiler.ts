67f3907a805d7cf83f1a23ddcb34a6f2966cb212
Vladimir Arsov <vladimir.arsov@collaboration-factory.de>
2025-02-28 08:55:20 +0100

    Generate a less file with paths to all dependency plugins

--- src/model/AssetsCompiler.ts
+++ src/model/AssetsCompiler.ts
@@ -356,6 +356,13 @@ export class AssetsCompiler {
                     cwarn`[${project.pluginName}] E2E assets are no longer compiled! Starting from the cplace release 23.1 all the E2E tests should be moved into a dedicated E2E repository. In addition, E2E tests must be written using Cypress!`
                 );
             }
+
+            if (project.hasLessAssets) {
+                project.generateCplacePluginsLess(
+                    (p) => projects.get(p),
+                    this.runConfig.localOnly
+                );
+            }
         });
 
         this.projects = projects;
@@ -580,7 +587,7 @@ export class AssetsCompiler {
                     'node_modules',
                     '@cplace-assets',
                     `${pluginDescriptor.repoName}_${pluginDescriptor.name
-                        .replaceAll('.', '-')
+                        .replace(/\./gi, '-')
                         .toLowerCase()}`
                 );
 
