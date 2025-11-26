c1f6cfe8ad882b021fd7282f7d6d941fc7c90011
Senthanal Sirpi Manohar <senthanal.sirpi.manohar@cplace.com>
2025-11-26 18:11:29 +0100

    PFM-ISSUE-31645 fix pipeline

--- test/e2e/assets-compiler.e2e.test.ts
+++ test/e2e/assets-compiler.e2e.test.ts
@@ -26,7 +26,7 @@ describe('Assets Compiler E2E Tests', () => {
     });
 
     describe('TC1: Verify assets compiler basic functionality', () => {
-        it.only(
+        it(
             'should verify assets compiler basic functionality with -c flag',
             () => {
                 const args = ['-c'];
